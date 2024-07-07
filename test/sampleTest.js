ca1258374af5826690f69260985301ff08e07cfb
luis <tiktokghs3@gmail.com>
2024-07-07 15:08:52 -0500

    sampleTest actualizacion

--- test/sampleTest.js
+++ test/sampleTest.js
@@ -1,7 +1,9 @@
 var assert = require('assert');
 
-describe('Sample Test', function() {
-  it('should return true', function() {
-    assert.strictEqual(true, true);
+describe('Array', function() {
+  describe('#indexOf()', function() {
+    it('should return -1 when the value is not present', function() {
+      assert.equal([1, 2, 3].indexOf(4), -1);
+    });
   });
 });
